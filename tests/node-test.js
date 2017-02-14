e2f98b67b247feceb73b564f6f18106f59911d8c
Michael Fenwick <mickyfen17@aol.com>
2017-02-13 17:01:18 -0700

    add basic first tests to constructors

--- tests/node-test.js
+++ tests/node-test.js
@@ -1,1 +1,17 @@
-import { expect, assert } from 'chai';
+import { expect, assert } from "chai";
+import Trie from "../lib/Trie";
+import Node from "../lib/Node";
+
+describe("Testing the node constructor", () => {
+
+  it("node should be a function", () => {
+    assert.isFunction(Node);
+  });
+
+  it("node should be a constructor", () => {
+    let node = new Node();
+
+    expect(node).to.be.instanceof(Node);
+  });
+
+});
