b7eab3f84000ff1467b12e7aa5e4b2b922e1a3b2
Michael Fenwick <mickyfen17@aol.com>
2017-02-16 14:36:25 -0700

    trying to solve suggest function

--- tests/trie-test.js
+++ tests/trie-test.js
@@ -6,31 +6,32 @@ import fs from "fs";
 let dictionary = fs.readFileSync('/usr/share/dict/words', 'utf-8').toLowerCase().trim().split('\n');
 
 describe("Testing the trie constructor", () => {
+  let trie;
+
+  beforeEach(() => {
+    trie = new Trie();
+  });
 
   it("trie should be a function", () => {
     assert.isFunction(Trie);
   });
 
   it("trie should be a constructor", () => {
-    let trie = new Trie();
 
     expect(trie).to.be.instanceof(Trie);
   });
 
   it("trie root should be an empty object as default", () => {
-    let trie = new Trie();
 
     expect(trie.root).to.be.an("object");
   });
 
   it("trie count should be 0 as default", () => {
-    let trie = new Trie();
 
     expect(trie.count).to.equal(0);
   });
 
   it("should be able to add a word and increase the count", () => {
-    let trie = new Trie();
 
     expect(trie.count).to.equal(0);
     trie.insert("test");
@@ -38,14 +39,12 @@ describe("Testing the trie constructor", () => {
   });
 
   it("should be able to add a letter and check is it exists in the nodes data key", () => {
-    let trie = new Trie();
     trie.insert("bob");
 
     expect(trie.root.b.data).to.equal("b");
   });
 
   it("should be able to add a word but check is it exists in the nodes data key", () => {
-    let trie = new Trie();
     trie.insert("bob");
 
     expect(trie.root.b.data).to.equal("b");
@@ -54,14 +53,12 @@ describe("Testing the trie constructor", () => {
   });
 
   it("should be able to add a letter into the tree", () => {
-    let trie = new Trie();
     trie.insert("a");
 
     expect(trie.root.a).to.have.deep.property("data", "a");
   });
 
   it("should be able to add a 2 letters into the tree", () => {
-    let trie = new Trie();
     trie.insert("ab");
 
     expect(trie.root.a).to.have.deep.property("children").
@@ -69,7 +66,6 @@ describe("Testing the trie constructor", () => {
   });
 
   it("should be able to add a two similar strings and check if they exist", () => {
-    let trie = new Trie();
     trie.insert("abc");
     trie.insert("abef");
 
@@ -80,7 +76,6 @@ describe("Testing the trie constructor", () => {
   });
 
   it("should be able to add a word but check when word is incomplete", () => {
-    let trie = new Trie();
     trie.insert("bob");
 
     expect(trie.root.b).to.have.deep.property("children").
@@ -91,7 +86,6 @@ describe("Testing the trie constructor", () => {
   });
 
   it("should be able to add a word and set when word is complete", () => {
-    let trie = new Trie();
     trie.insert("bob");
 
     expect(trie.root.b).to.have.deep.property("children").
@@ -103,15 +97,18 @@ describe("Testing the trie constructor", () => {
 
   });
 
-  it.skip("testing suggest", () => {
-    let trie = new Trie();
-    trie.insert("bob");
-    trie.suggest("b");
+  it.only("suggest should return and array of matched words", () => {
+    trie.insert("apple");
+    trie.insert("applicant");
+    trie.insert("application");
+    trie.suggest("a");
+
+    console.log("returned array", trie.suggestArray);
+
+    expect(trie.suggestArray).to.equal(["apples", "applicant", "application"]);
   });
 
   it.skip("testing trie", () => {
-    let trie = new Trie();
-
     trie.insert("word");
     trie.insert("world");
     trie.insert("worry");
@@ -120,16 +117,12 @@ describe("Testing the trie constructor", () => {
   });
 
   it("populate be able to accept arrays and increase the word count", () => {
-    let trie = new Trie();
-
     trie.populate(["test", "test2"]);
 
     expect(trie.count).to.equal(2);
   });
 
   it("populate be able to accept arrays and push into tree via insert", () => {
-    let trie = new Trie();
-
     trie.populate(["test"]);
 
     expect(trie.root.t).to.have.deep.property("children").
@@ -143,16 +136,12 @@ describe("Testing the trie constructor", () => {
   });
 
   it("populate be able to accept the dictionary in array form and log a word count of 235886", () => {
-    let trie = new Trie();
-
     trie.populate(dictionary);
 
     expect(trie.count).to.equal(235886);
   });
 
   it("populate be able to accept the dictionary and be able to search for a random word", () => {
-    let trie = new Trie();
-
     trie.populate(dictionary);
 
     expect(trie.root.b.children.a.children.l.children.l).
