4d106ccade75ebc7fcfc2ccad8c7b815d0929e54
Michael Fenwick <mickyfen17@aol.com>
2017-02-16 19:39:50 -0700

    add a before each function to create new Node

--- tests/node-test.js
+++ tests/node-test.js
@@ -2,27 +2,29 @@ import { expect } from "chai";
 import Node from "../lib/Node";
 
 describe("Testing the node constructor", () => {
+  let node;
+
+  beforeEach(() => {
+    node = new Node();
+  }) ;
 
   it("an initialized node has the expected default state", () => {
-    let node = new Node();
 
     expect(node).to.be.instanceof(Node);
   });
 
   it("node data should be able to accept and store a value", () => {
-    let node = new Node("test");
+    node = new Node("test");
 
     expect(node.data).to.equal("test");
   });
 
   it("node isWord should be false as default", () => {
-    let node = new Node();
 
     expect(node.isWord).to.equal(false);
   });
 
   it("node children should be an object", () => {
-    let node = new Node();
 
     expect(node.children).to.be.an("object");
   });
